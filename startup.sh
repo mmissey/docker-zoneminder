7c250e43ca0bfc05f5f321c872efd63cb3ce6e63
Marc Missey <marcmissey@gmail.com>
2018-05-27 22:47:18 -0600

    fix: typo in startup script

--- startup.sh
+++ startup.sh
@@ -40,8 +40,8 @@ else
         
         chown -R root:www-data /var/cache/zoneminder /etc/zm/zm.conf
         chmod -R 770 /var/cache/zoneminder /etc/zm/zm.conf
-        echo "SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION';" | mysql -u root -pmysqlpsswd -h $ZM_DB_HOST
-        mysql -u root -pmysqlpsswd -h $ZM_DB_HOST < /usr/share/zoneminder/db/zm_create.sql   
+        echo "SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION';" | mysql -u root -p mysqlpsswd -h $ZM_DB_HOST
+        mysql -u root -p mysqlpsswd -h $ZM_DB_HOST < /usr/share/zoneminder/db/zm_create.sql   
         date > /var/cache/zoneminder/dbcreated
         #needed to fix problem with ubuntu ... and cron 
         update-locale
